eb7a5b4806e0d353f3cad98915b3198ee9690310
Jacques Estevan Siman <jacao010706@gmail.com>
2026-09-19 15:33:53 -0300

    chore: limpar arquivos temporários de patch

--- frontend/src/app/cco/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function CCOLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div style={{ width: "100vw", height: "100vh", overflow: "hidden", background: "#050505" }}>
-      {children}
-    </div>
-  );
-}
